6415a2639eb2ba5547139f58f08b61689db2d876
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-01-20 12:21:41 +0200

    adding create  and redirect

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import movies from '../movies.js';
+import movieService from '../services/movieService.js';
 const router = Router();
 
 router.get('/', (req, res) => {
+    const movies = movieService.getAll();
+    
     res.render('home',{ movies });
 })
 router.get('/about', (req, res) => {
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -10,9 +10,10 @@ movieController.get('/create',(req,res)=>{
 });
 movieController.post('/create',(req,res)=>{
     const newMovie = req.body;
+
+    movieService.create(newMovie);
     
-    
-    res.end();
+    res.redirect('/')
 });
 movieController.get('/:movieId/details',(req,res)=>{
     const movieId = req.params.movieId;
