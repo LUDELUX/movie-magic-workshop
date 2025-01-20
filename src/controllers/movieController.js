edb36a1805691399ca1eb547b41ef9e835a6d7f2
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-01-20 10:01:37 +0200

    add details page with hard coded markup

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -5,5 +5,7 @@ const movieController = Router();
 movieController.get('/create',(req,res)=>{
     res.render('create');
 });
-movieController.get('/:movieId/details')
+movieController.get('/:movieId/details',(req,res)=>{
+    res.render('details')
+})
 export default movieController;
