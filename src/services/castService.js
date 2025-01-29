bce6219833b5a45e23f1ed408436a912bcee8111
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-01-29 16:44:30 +0200

    exclude casts that are already attached

--- src/services/castService.js
+++ src/services/castService.js
@@ -1,7 +1,12 @@
 import Cast from "../models/Cast.js";
 export default {
-    getAll(ids){
-        return Cast.find({});
+    getAll(filter = {}){
+        let query = Cast.find({});
+
+        if (filter.exclude) {
+            query = query.find({_id: {$nin: filter.exclude}});
+        }
+        return query;
         //  return Cast.find({_id: {$in: ids}});
     },
     create(castData){
