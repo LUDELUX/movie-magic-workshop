452300a2625129f60569a6ad2ee0ffcaf26d3199
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-02-01 13:38:22 +0200

    add login functionaliti

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -19,6 +19,20 @@ authController.get('/login',(req,res)=>{
     res.render('auth/login');
     
 });
+authController.post('/login', async (req,res)=>{
+    const {email, password} = req.body;
+
+    try {
+       const token= await authService.login(email,password);
+       console.log(token);
+       
+    } catch (error) {
+        return res.redirect('/404')
+    }
+
+    res.redirect('/');
+    
+});
 
 
 export default authController;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,31 @@
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = '$zs$32$s.QWJavtRYvmKfjtgo/eRuVtGl.B2H71Gm1Vq5X9HFYl8qxVG4a9.';
 
 export default {
     register(userData){
         return User.create(userData)
+    },
+    async login(email,password){
+        const user = await User.findOne({email});
+        if (!user) {
+            throw new Error('Invalid email or password!');
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if(!isValid){
+            throw new Error('Invalid email or password!');
+        }
+
+        const payLoad = {
+            id: user.id,
+            email:user.email, 
+        }
+        const token = jwt.sign(payLoad,SECRET, {expiresIn: '2h'});
+
+
+        return token;
     }
 }
