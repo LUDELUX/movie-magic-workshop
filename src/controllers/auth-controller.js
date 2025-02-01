3165bcae5eb49551040b61079311f2754f92e6db
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-02-01 09:41:58 +0200

    update navigation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const authController = Router();
 
+authController.get('/register',(req,res)=>{
+    res.render('auth/register');
+
+});
+
+
 export default authController;
