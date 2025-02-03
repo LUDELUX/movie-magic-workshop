918368a898c18ceba467d70fdcb3ba351c30b130
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-02-03 14:41:38 +0200

    add logout

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -35,6 +35,11 @@ authController.post('/login', async (req,res)=>{
     
     
 });
+authController.get('/logout', (req,res)=> {
+    res.clearCookie('auth');
+
+    res.redirect('/');
+})
 
 
 export default authController;
