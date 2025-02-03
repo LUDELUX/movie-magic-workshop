e8a8c40163f337066d98b54c1d8b1dee3786257a
LUDELUX <92664218+LUDELUX@users.noreply.github.com>
2025-02-03 14:54:56 +0200

    add dinamyc navigation

--- src/middlewares/auth-middlewares.js
+++ src/middlewares/auth-middlewares.js
@@ -12,6 +12,7 @@ export const authMiddleware = (req,res,next)=>{
         const decodedToken = jwt.verify(token,SECRET);
 
         req.user = decodedToken;
+        res.locals.user = decodedToken;
         next();
     } catch (err) {
         res.clearCookie('auth');
